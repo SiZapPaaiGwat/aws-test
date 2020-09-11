9b1974c9903445f228de4fe760fce14dffd7e5c0
世界之极 <xugaofan@gmail.com>
2020-09-11 15:27:55 +0800

    Add source code

---
+++ website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-2_Rkm5qS710', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3sum377kneo2n0vjcf21mu9ock', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
